e4c0dc984a5834d87ab620962387d7ae201365d1
Steven <beltondavid1@gmail.com>
2020-03-20 16:48:14 +0800

    Add images

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -47,7 +47,15 @@ class Home extends StatelessWidget {
             ),
           ],
         ),
-        body: BrewList(),
+        body: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/coffee_bg.png'),
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: BrewList(),
+        ),
       ),
     );
   }
