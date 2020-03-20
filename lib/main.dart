e5a967c80ed3ee40e9db8c3c029287ce8f5050b8
Steven <beltondavid1@gmail.com>
2020-03-20 16:00:24 +0800

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_with_firebase/models/user.dart';
+import 'package:flutter_with_firebase/screens/wrapper.dart';
+import 'package:flutter_with_firebase/services/auth.dart';
+import 'package:provider/provider.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return StreamProvider<User>.value(
+      value: AuthService().user,
+      child: MaterialApp(
+        home: Wrapper(),
+      ),
+    );
+  }
+}
